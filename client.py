f39f4308bca66816a275af59ed67013d28240e13
JEONGJAEUK <sangs626@gmail.com>
2022-11-15 08:52:58 +0900

    version 2

--- client.py
+++ client.py
@@ -3,52 +3,118 @@ import ssl
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5 as Cipher_PKCS1_v1_5
 from base64 import b64encode, b64decode
+import os
+import socket
+import ssl
+import hashlib
+import getpass
 
 
-HOST = '192.168.137.207'
-PORT = 8080
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+def set_context():
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)  # openSSL 에서 TLS 를 사용할 것임을 선언
+    context.load_verify_locations('CACert.pem')  # 서버의 인증서를 등록(자기 서명 인증서의 경우 등록하지 않으면 에러 발생)
+    if os.path.isfile("Cert.pem") and os.path.isfile("Priv.pem"):
+        context.load_cert_chain("Cert.pem", "Priv.pem")  # 사용자의 인증서 및 개인 키 등록
+    return context
+
+
+def sign_up(s):
+    s.send("sign_up".encode())
+    data = s.recv(1024).decode()
+    print(data)
+    data = s.recv(1024).decode()
+    print(data)
+    while True:
+        s_data = str(input("메시지 : "))
+        if s_data == "quit":
+            s.sendall(s_data.encode())
+            print("Cert Process Stopped")
+            return
+        s.sendall(s_data.encode())
+        r_data = s.recv(1024)
+        if r_data.decode() == "END":
+            print('Received', repr(r_data.decode()))
+            get_file(s, "Cert")
+            get_file(s, "Priv")
+            print("Sign up End")
+            print("Restart Program")
+            break
+        print('Received', repr(r_data.decode()))
+
+
+def sign_in(s):
+    s.send("sign_in".encode())
+    while True:
+        data = s.recv(1024).decode()
+        print(data)
+        userID = input(" : ")
+        s.send(userID.encode())
+        data = s.recv(1024).decode()
+        print(data)
+        if data != "Not found":
+            while True:
+                userPW = getpass.getpass()
+                hashPW = hashlib.sha256(userPW.encode()).hexdigest()
+                s.send(hashPW.encode())
+                data = s.recv(1024).decode()
+                if data != "Wrong Password":
+                    get_file(s, "Cert")
+                    get_file(s, "Priv")
+                    print("Sign in End")
+                    print("Restart Program")
+                    break
+            break
+
+
+def get_file(conn, filename):
+    with open(filename + ".pem", 'wb') as f:
+        try:
+            r_data = conn.recv(1024)
+            while r_data.decode() != "END":
+                f.write(r_data)
+                r_data = conn.recv(1024)
+        except Exception as e:
+            print(e)
+
+
+def connection(context):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:  # TCP 소켓 생성
+        with context.wrap_socket(sock, server_hostname='JeongJaeUk',
+                                 do_handshake_on_connect=False) as s:  # TCP 소켓 위에 TLS 소켓 생성
+            s.connect((HOST, PORT))  # 서버에 접속
+            s.do_handshake()  # 핸드 셰이크 실행
+            # 이하 프로그램 실행 코드
+            while True:
+                if not os.path.isfile("Cert.pem") and not os.path.isfile("Priv.pem"):  # 사용자의 Cert 및 개인키가 없을 때
+                    while True:
+                        sign_option = int(input("1. Sign up\n2. Sign in\n: "))
+                        if sign_option == 1:
+                            sign_up(s)
+                            return True
+                        elif sign_option == 2:
+                            sign_in(s)
+                            return True
+                        else:
+                            print("Wrong commands")
+                s_data = input("Input message : ")
+                if s_data == "quit" or s_data == "exit" or s_data == "end":
+                    return False
+                else:
+                    s.sendall(s_data.encode('utf-8'))
+                    r_data = s.recv(1024).decode('utf-8')
+                    print(f'서버응답 : {r_data}')
 
 
 def run():
-    while True:
-        data = str(input("메시지 : "))
-        if data == "quit":
-            break
-        else:
-            client_socket.sendall(data.encode())
-            if data == "get_cert":
-                get_cert()
-            elif data == "help":
-                data = client_socket.recv(1024)
-                print(data.decode())
-            else:
-                data = client_socket.recv(1024)
-                print(data.decode())
-
-
-def get_cert():
-    data = client_socket.recv(1024).decode()
-    print(data)
-    while True:
-        data = str(input("메시지 : "))
-        client_socket.sendall(data.encode())
-        data = client_socket.recv(1024)
-        if data.decode() == "END":
-            print('Received', repr(data.decode()))
-            break
-        print('Received', repr(data.decode()))
-
-
-def gen_key():
-    private_key = RSA.generate(2048)
-    public_key = private_key.publickey()
-    with open("PrivKey.pem", "wb+") as f:
-        f.write(private_key.export_key('PEM'))
-    with open("PubKey.pem", "wb+") as f:
-        f.write(public_key.export_key('PEM'))
+    context = set_context()
+    is_running = connection(context)
+    return is_running
 
 
 if __name__ == "__main__":
-    run()
+    HOST = '192.168.137.207'  # 서버 IP
+    PORT = 8080  # 서버 Port
+    running = True
+    while running:
+        running = run()
+    print("Disconnect from Server : " + HOST + ":" + str(PORT))
--- server.py
+++ server.py
@@ -1,111 +1,198 @@
-import socket
-import ssl
-import random
-import subprocess
+import socket, ssl, datetime, _thread, os, re
 from OpenSSL import crypto
-from _thread import *
-import os, re, datetime
 
 
 def get_help():
     message = "Manual\n1. get_cert : Getting Your Cert\n0. quit : disconnect"
-    client_socket.send(message.encode())
+    conn.send(message.encode())
+
+
+def send_subject_message(subject):
+    message = "input your " + subject
+    conn.send(message.encode())
+    print("Input " + subject + " Mode")
+    data = conn.recv(1024)
+    if data.decode() == "quit":
+        return False
+    else:
+        return data.decode()
 
 
 def get_cert_req():
+    conn.send("Sign up Process Starting...".encode())
     print("Start Getting Cert Mode")
-    psec = crypto.PKey()
-    psec.generate_key(crypto.TYPE_RSA, 2048)
     req = crypto.X509Req()
     subject = req.get_subject()
     try:
-        client_socket.send("Start gertting Cert\ninput your Name".encode())
-        print("Input Name")
-        name = client_socket.recv(1024)
-        if os.path.isdir("Cert/" + name.decode()):
-            client_socket.send("Already exists name".encode())
-            return
+        user_id = send_subject_message("id")
+        print(user_id)
+        if user_id:
+            if os.path.isdir("Cert/" + user_id):
+                conn.send("Already exists id".encode())
+                return
         else:
-            os.makedirs("Cert/" + name.decode())
+            print("quit message from User")
+            return
 
-        subject.CN = name.decode()
+        data = send_subject_message("Contry")
+        if data:
+            subject.C = data
+        else:
+            print("quit message from User")
+            return
 
-        client_socket.send("input your Country".encode())
-        print("Input Country")
-        data = client_socket.recv(1024)
-        subject.C = data.decode()
+        data = send_subject_message("State")
+        if data:
+            subject.ST = data
+        else:
+            print("quit message from User")
+            return
 
-        client_socket.send("input your State".encode())
-        print("Input State")
-        data = client_socket.recv(1024)
-        subject.ST = data.decode()
+        data = send_subject_message("Office")
+        if data:
+            subject.O = data
+        else:
+            print("quit message from User")
+            return
 
-        client_socket.send("input your Office".encode())
-        print("Input Office")
-        data = client_socket.recv(1024)
-        subject.O = data.decode()
-
-        client_socket.send("input your e-mail".encode())
-        print("Input e-mail")
-        data = client_socket.recv(1024)
         p = re.compile("\w+@\w+[.]\w+")
-        while not p.match(data.decode()):
-            client_socket.send("Invalid format - ex)aa@bb.cc".encode())
-            print("Input e-mail")
-            data = client_socket.recv(1024)
-        subject.emailAddress = data.decode()
+        data = send_subject_message("e-mail")
+        if data:
+            while not p.match(data):
+                conn.send("Invalid format - ex)aa@bb.cc".encode())
+                print("Input e-mail")
+                data = conn.recv(1024).decode()
+            subject.emailAddress = data
+        else:
+            print("quit message from User")
+            return
 
+        psec = crypto.PKey()
+        psec.generate_key(crypto.TYPE_RSA, 2048)
         req.set_pubkey(psec)
         req.sign(psec, "sha256")
-        with open("Cert/" + name.decode() + "/" + now + "-" + name.decode() + ".pem", "wb") as f:
-            f.write(crypto.dump_certificate_request(crypto.FILETYPE_PEM, req))
-        client_socket.send("END".encode())
+        filename = now + "-" + user_id
+        os.makedirs("Cert/" + user_id)
+        with open("Cert/" + user_id + "/Priv.pem", "wb") as f:
+            f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, psec))
+        subject.CN = user_id
+        get_cert(req, user_id, filename)
+        conn.send("END".encode())
+        trans_cert(conn, user_id, "Cert")
+        conn.send("END".encode())
+        trans_cert(conn, user_id, "Priv")
+        conn.send("END".encode())
         print("End Getting Cert Mode")
     except ConnectionResetError as e:
         print('Disconnected by ' + addr[0], ':', addr[1])
 
 
-def get_cert():
-    req = crypto.X509Req()
+def get_cert(req, user_id, filename):
+    with open("serial_number.txt", "r") as f:
+        number = int(f.read()) + 1
+
+    with open("serial_number.txt", "w") as f:
+        f.write(str(number))
+    cert = crypto.X509()
+    cert.set_serial_number(number)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(365 * 24 * 60 * 60)
+    cert.set_subject(req.get_subject())
+    cert.set_issuer(CACert.get_subject())
+    cert.set_pubkey(req.get_pubkey())
+    cert.sign(CAPriv, "sha256")
+
+    with open("Cert/" + user_id + "/Cert.pem", "wb") as f:
+        f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
 
 
-def threaded(client_socket, addr):
+def trans_cert(conn, user_id, filename):
+    print(filename + " transmission Start")
+    with open("Cert/" + user_id + "/" + filename + ".pem", 'rb') as f:
+        try:
+            data = f.read(1024)
+            while data:
+                conn.send(data)
+                data = f.read(1024)
+        except Exception as e:
+            print(e)
+
+
+def send_cert():
+    while True:
+        conn.send("input your ID".encode())
+        user_id = conn.recv(1024).decode()
+        if os.path.isdir("Cert/" + user_id):
+            conn.send("input your Password".encode())
+            user_pw = conn.recv(1024).decode()
+            with open("Cert/" + user_id + "/password.txt", "r") as f:
+                open_pw = f.read()
+                if user_pw == open_pw:
+                    conn.send("Transport Mode".encode())
+                    trans_cert(conn, user_id, "Cert")
+                    conn.send("END".encode())
+                    trans_cert(conn, user_id, "Priv")
+                    conn.send("END".encode())
+                    break
+                else:
+                    conn.send("Wrong Password".encode())
+        else:
+            conn.send("Not Found".encode())
+
+
+def tls_threaded(conn, addr):
     print('Connected by :', addr[0], ':', addr[1])
+    if conn.getpeercert() is None:
+        data = conn.recv(1024).decode()
+        if data == "sign_up":
+            get_cert_req()
+        elif data == "sign_in":
+            send_cert()
+    else:
+        peercert = conn.getpeercert()
+        user_name = peercert['subject'][4][0][1]
     while True:
         try:
-            data = client_socket.recv(1024)
+            data = conn.recv(1024).decode()
             if not data:
                 print('Disconnected by ' + addr[0], ':', addr[1])
                 break
             else:
-                print('Received from ' + addr[0], ':', addr[1], "message : " + data.decode())
-                if data.decode() == "get_cert":
-                    get_cert_req()
-                elif data.decode() == "help":
+                print('Received from ' + addr[0], ':', addr[1], data)
+                if data == "help":
                     get_help()
                 else:
-                    client_socket.send("Not Found".encode())
-
+                    conn.send("Not Found".encode())
+                print("wait")
         except ConnectionResetError as e:
             print('Disconnected by ' + addr[0], ':', addr[1])
             break
-    client_socket.close()
+
+    conn.close()
 
 
 HOST = '192.168.137.207'
 PORT = 8080
+
 datetime.timezone(datetime.timedelta(hours=9))
 now = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+with open("CAPriv.pem", "rb+") as f:
+    CAPriv = crypto.load_privatekey(crypto.FILETYPE_PEM, f.read())
+with open("CACert.pem", "rb+") as f:
+    CACert = crypto.load_certificate(crypto.FILETYPE_PEM, f.read())
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((HOST, PORT))
-server_socket.listen()
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.verify_mode = ssl.CERT_OPTIONAL
+context.load_cert_chain('CACert.pem', 'CAPriv.pem')
+context.load_verify_locations("CACert.pem")
 
-print('server start')
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind((HOST, PORT))
+sock.listen()
+ssl_sock = context.wrap_socket(sock, server_side=True, do_handshake_on_connect=False)
 while True:
     print('wait')
-    client_socket, addr = server_socket.accept()
-    start_new_thread(threaded, (client_socket, addr))
-
-server_socket.close()
+    conn, addr = ssl_sock.accept()
+    conn.do_handshake()
+    _thread.start_new_thread(tls_threaded, (conn, addr))
